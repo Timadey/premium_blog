8a39f92e4fbe0636a5aec0ed003b1c2fa0d14b64
Timadey <dlktimothy@gmail.com>
2022-08-04 14:55:03 +0100

    [Update] header padding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
                             <path
                                 d="M6.999 2.6A5.5 5.5 0 0 1 15 7.5a.5.5 0 0 0 1 0 6.5 6.5 0 1 0-13 0 5 5 0 0 0 6.001 4.9A5.5 5.5 0 0 1 1 7.5a.5.5 0 0 0-1 0 6.5 6.5 0 1 0 13 0 5 5 0 0 0-6.001-4.9zM10 7.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" />
                         </svg>
-                        <span class="fs-4">{{ 'Premiun Blog' }}</span>
+                        <span class="fs-5 ms-2">{{ 'Premium Blog' }}</span>
                     </a>
 
                     <ul class="nav nav-pills">
